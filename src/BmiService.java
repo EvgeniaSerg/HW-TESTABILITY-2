8c6adff1d75e2e16d53e168f24501de496cbf124
Eugenia <st_shelkunova@list.ru>
2023-10-31 21:12:00 +0300

    src

--- src/BmiService.java
+++ src/BmiService.java
@@ -2,7 +2,7 @@ public class BmiService {
     public int calculate(double weight) {
         double result;
         double height = 1.87;
-        result = weight / ( height * height );
+        result = weight / (height * height);
         return (int) result;
     }
 }
